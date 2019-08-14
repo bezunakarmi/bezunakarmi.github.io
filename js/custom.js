36e392a0678f86d7b3f8ab20a6c1cb36e5624a2b
Biju Nakarmi <bezunakarmi@gmail.com>
2019-08-14 16:39:38 +0545

    Changed picture

--- js/custom.js
+++ js/custom.js
@@ -55,6 +55,9 @@ window.onload = function() {
         var toRotate = elements[i].getAttribute('data-type');
         var period = elements[i].getAttribute('data-period');
         if (toRotate) {
+
+
+            
           new TxtType(elements[i], JSON.parse(toRotate), period);
         }
     }
